f1a56f324cdc23065f40243ded720fc5a00bf09c
Serbah291 <na_benberrah@esi.dz>
2025-03-26 01:19:45 +0100

    added forget password / reset password functionnality to user

--- models/voyageModel.js
+++ models/voyageModel.js
@@ -1,76 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const voyageSchema = new mongoose.Schema({
-    title: {
+    name: {
         type: String,
-        required: true,
+        required: [true, 'Please provide a name for this voyage']
     },
-    codePromo: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "PromoCode",
-    },
-    subTitle: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    destination: {
-        type: String,
-        required: true,
-    },
-    prix: {
-        type: Number,
-        required: true,
-    },
-    dateDepart: {
-        type: Date,
-        default: Date.now,
-    },
-    placesDisponible: {
-        type: Number,
-        required: true,
-    },
-    imagePrincipale: {
-        type: String,
-        required: true,
-    },
-    images: [{
-        type: String,
-    }],
     category: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Category",
-        required: [true, "Please provide a category Id"],
+        ref: 'Category', // Référence au modèle CategoryModel
+        required: true
     },
-    subCategory: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "subCategory",
-        required: [true, "Please provide a subCategory Id"],
-    },
-    activities: [{
-        type: String,
-    }],
-    rating: {
+    price: {
         type: Number,
-        required: true,
-        min: 1,
-        max: 5,
+        required: [true, 'Please provide a price']
     },
+    description: String
 }, {
-    timestamps: true,
-    toJSON: {
-        virtuals: true
-    },
-    toObject: {
-        virtuals: true
-    }
+    timestamps: true
 });
 
+const VoyageModel = mongoose.model('Voyage', voyageSchema);
 
-
-const Voyage = mongoose.model("Voyage", voyageSchema);
-
-module.exports = Voyage;
+module.exports = VoyageModel;
